6e06d932be66d0860570f1029fefadf6297cbf57
Yashwanth <yashwanth7780@gmail.com>
2025-05-16 11:07:55 +0530

    Today building the password Authentication

---
+++ Major101/models/user.js
@@ -0,0 +1,17 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMangoose = require("passport-local-mongoose");
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+
+});
+
+UserActivation.plugin(passportLocalMangoose);
+
+module.exports = mongoose.model("User", userSchema);
