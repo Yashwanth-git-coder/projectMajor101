afc5e63c70dcd6bf68aa16bbe82bd4310f148868
Yashwanth <yashwanth7780@gmail.com>
2025-05-20 09:13:56 +0530

    All Map thinks done

--- Major101/controllers/listing.js
+++ Major101/controllers/listing.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -28,19 +32,41 @@ module.exports.showListings = async (req, res) => {
 };
 
 module.exports.createRoute = async (req, res) => {
-  let url = req.file.path;
-  let filename = req.file.filename;
+  try {
+    let response = await geocodingClient.forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1
+    }).send();
 
-  const newListing = new Listing(req.body.listing);
+    // Safe extraction of geometry from geocoding response
+    const geometry = response.body.features?.[0]?.geometry;
 
-  newListing.owner = req.user._id;
-  newListing.image = {url, filename};
+    if (!geometry) {
+      req.flash("error", "Location not found. Please enter a valid location.");
+      return res.redirect("/listings/new");
+    }
 
-  await newListing.save();
-  req.flash("success", "New Listing Created!");
-  res.redirect("/listings");
+    let url = req.file?.path || "";
+    let filename = req.file?.filename || "";
+
+    const newListing = new Listing(req.body.listing);
+
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+    newListing.geometry = geometry;
+
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+
+  } catch (error) {
+    console.error("Error creating listing:", error);
+    req.flash("error", "Something went wrong while creating the listing.");
+    res.redirect("/listings/new");
+  }
 };
-
 
 
 module.exports.editRoute = async (req, res) => {
--- Major101/public/js/map.js
+++ Major101/public/js/map.js
@@ -1,6 +1,11 @@
 mapboxgl.accessToken = mapTOKEN;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
-center: [77.5764, 12.8987], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-zoom: 9 // starting zoom
+center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+zoom: 7 // starting zoom
 });
+
+// Create a default Marker and add it to the map.
+const marker = new mapboxgl.Marker({color: 'red'})
+    .setLngLat(coordinates)
+    .addTo(map);
